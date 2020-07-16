d6c64bb3790f29cf5de378b705294bbda907ca51
ayush15gupta <smileayushgupta@gmail.com>
2020-07-16 11:07:16 +0530

    Mongoose Population

--- models/user.js
+++ models/user.js
@@ -2,6 +2,14 @@ const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 var User=new Schema({
+    firstname: {
+        type: String,
+          default: ''
+      },
+      lastname: {
+        type: String,
+          default: ''
+      },
     
     admin:   {
         type: Boolean,
